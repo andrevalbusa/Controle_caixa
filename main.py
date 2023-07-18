1eeb56a12f89f76319abe33044581521fe22b9fb
Andre Valbusa <71150639+andrevalbusa@users.noreply.github.com>
2023-07-18 15:40:51 -0300

    Função Extração de Código de Barras
    Adição da função que utiliza o código de barras para preencher as informações que serão adicionadas ao banco de dados.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from typing import Optional
+from datetime import datetime, timedelta
 from PyQt5 import QtWidgets, uic
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
@@ -15,8 +16,40 @@ from PySide6.QtWidgets import (QApplication, QCalendarWidget, QCheckBox, QComboB
     QTabWidget, QTableWidget, QTableWidgetItem, QTextEdit,
     QToolBox, QVBoxLayout, QWidget)
 from layout_CC import Ui_MainWindow
-import funcoes
+#import funcoes
 
+def extrair_codigo():
+    codigo = ui.cod_boleta.text()
+    cod_inst = codigo[0:3]
+    ################ DATA DE VENCIMENTO ################
+    cod_vencimento = int(codigo[31:35])
+    data_base = '07/10/1997'
+    formato_data = "%d/%m/%Y"
+    data_base_obj = datetime.strptime(data_base, formato_data)
+    data_calculada = data_base_obj + timedelta(days=cod_vencimento)
+    ano_vencimento = data_calculada.year
+    mes_vencimento = data_calculada.month
+    dia_vencimento = data_calculada.day
+    data = QDate(ano_vencimento, mes_vencimento, dia_vencimento)
+    ui.vencimento_boleta.setDate(data)
+
+    #################### VALOR BOLETO ##################
+    cod_reais = int(codigo[35:43])
+    reais_format = str(cod_reais)
+    cod_centavos = codigo[43:45]
+    valor_boleto = (reais_format + ',' + cod_centavos)
+    ui.valor_boleta.setText(valor_boleto)
+
+    ##################### FORNECEDOR ###################
+    
+    ################### LIMPAR CODIGO ##################
+    ui.cod_boleta.clear()
+
+    return 
+
+def gravar_boleto():
+    #gravar informações no banco de dados sqlite
+    return
 
 class MainWindow(QMainWindow, Ui_MainWindow):  
 
@@ -31,8 +64,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.btn_consultar.clicked.connect(lambda: self.Pages.setCurrentWidget(self.pag_consultar))
 
         ################################### EXTRACAO DE INFORMAÇÕES DO COD DE BARRAS ##################################################
-        self.btn_extrair.clicked.connect(funcoes.extrair_codigo)
-
+        self.btn_extrair.clicked.connect(extrair_codigo)
+        self.btn_adicionar.clicked.connect(gravar_boleto)
 
 
 if __name__ == '__main__':
